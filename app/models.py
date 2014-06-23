511365d270bc735cb26a14e1700a5e2a98877bbb
Madis Veskimeister <madis@pingviinitiivul.ee>
2014-06-23 22:31:32 +0000

    Added table names, some constructors, Postgres requirement.

--- app/models.py
+++ app/models.py
@@ -8,12 +8,23 @@ from app import app, db
 from datetime import datetime
 from sqlalchemy.orm import relationship, backref
 
+shippartowners = db.Table('shippartowners',
+                                    db.Column('ship.id', db.Integer,
+                                              db.ForeignKey('ship.id')),
+                                    db.Column(
+                                        'shippart.id',
+                                        db.Integer,
+                                        db.ForeignKey('shippart.id'))
+                                    )
+
 class Faction(db.Model):
+    __tablename__    = 'faction'
     id          = db.Column(db.Integer, primary_key=True)
     name        = db.Column(db.String(), unique=True, nullable=True)
     race        = db.Column(db.String(), unique=True, nullable=True)
 
 class Player(db.Model):
+    __tablename__    = 'player'
     id          = db.Column(db.Integer, primary_key=True)
     name        = db.Column(db.String(), unique=True, nullable=True)
     time_added_to_base = db.Column(db.DateTime(timezone=True), default=db.func.now())
@@ -21,31 +32,78 @@ class Player(db.Model):
     faction     = db.relationship("Faction", backref="player", lazy="joined")
 
 class Body(db.Model):
+    __tablename__    = 'body'
     id          = db.Column(db.Integer, primary_key=True)
     coordX      = db.Column(db.Float)
     coordY      = db.Column(db.Float)
+    mass        = db.Column(db.Float)
+
+    def __init__(self, coordX, coordY, mass):
+        self.coordX = coordX
+        self.coordY = coordY
+        self.mass   = mass
 
 class Star(db.Model):
+    __tablename__    = 'star'
     id          = db.Column(db.Integer, primary_key=True)
     name        = db.Column(db.String(), unique=True, nullable=False)
     body_id     = db.Column(db.Integer, db.ForeignKey('body.id'), nullable=False, index=True, unique=True)
     body        = db.relationship('Body', uselist=False, backref=db.backref('star', uselist=False), lazy='joined')
 
+    def __init__(self, name, coordX, coordY, mass):
+        self.name = name;
+        self.body = Body(coordX, coordY, mass);
+
 class Planet(db.Model):
+    __tablename__    = 'planet'
     id          = db.Column(db.Integer, primary_key=True)
     name        = db.Column(db.String(), unique=True, nullable=False)
     body_id     = db.Column(db.Integer, db.ForeignKey('body.id'), nullable=False, index=True, unique=True)
     body        = db.relationship('Body', uselist=False, backref=db.backref('planet', uselist=False), lazy='joined')
+    star_id     = db.Column(db.Integer, db.ForeignKey('star.id'), nullable=False, index=True, unique=True)
+    star        = db.relationship('Star', backref=db.backref('star'), lazy='joined')
 
-class Ship(db.Model):
+
+class ShipClass(db.Model):
+    __tablename__    = 'shipclass'
     id          = db.Column(db.Integer, primary_key=True)
     name        = db.Column(db.String(), unique=True, nullable=False)
-    faction_id  = db.Column(db.Integer, db.ForeignKey('faction.id'), nullable=False)
-    faction     = db.relationship("Faction", backref="ships", lazy="joined")
-    body_id     = db.Column(db.Integer, db.ForeignKey('body.id'), nullable=False, index=True, unique=True)
-    body        = db.relationship('Body', uselist=False, backref=db.backref('ship', uselist=False), lazy='joined')
+    #ships       = db.relationship("Ship", backref="shipclass", lazy="dynamic")
 
+class ShipPartClass(db.Model):
+    __tablename__    = 'shippartclass'
+    id               = db.Column(db.Integer, primary_key=True)
+    name             = db.Column(db.String(), unique=True, nullable=False)
+
+class ShipPart(db.Model):
+    __tablename__    = 'shippart'
+    id               = db.Column(db.Integer, primary_key=True)
+    shippartclass_id = db.Column(db.Integer, db.ForeignKey('shippartclass.id'), nullable=False)
+    shippartclass    = db.relationship('ShipPartClass', backref=db.backref('shippart', uselist=False), lazy='joined')
+    maxstructure     = db.Column(db.Integer)
+    curstructure     = db.Column(db.Integer)
+
+class Ship(db.Model):
+    __tablename__    = 'ship'
+    id           = db.Column(db.Integer, primary_key=True)
+    name         = db.Column(db.String(), unique=True, nullable=False)
+    faction_id   = db.Column(db.Integer, db.ForeignKey('faction.id'), nullable=False)
+    faction      = db.relationship("Faction", backref="ships", lazy="joined")
+    body_id      = db.Column(db.Integer, db.ForeignKey('body.id'), nullable=False, index=True, unique=True)
+    body         = db.relationship('Body', uselist=False, backref=db.backref('ship', uselist=False), lazy='joined')
+    shipclass_id = db.Column(db.Integer, db.ForeignKey('shipclass.id'), nullable=False, index=True, unique=True)
+    shipclass    = db.relationship('ShipClass', backref=db.backref('ship', uselist=False), lazy='joined')
+    #ship parts#
+    shipparts    = db.relationship("ShipPart", backref="ship", secondary="shippartowners", lazy="dynamic")
+
+    @property
+    def maxstructure(self):
+        maxstr = 0
+        for part in self.shipparts:
+            maxstr = maxstr + part.maxstructure
+        return maxstr
 
 class Game(db.Model):
+    __tablename__    = 'game'
     id          = db.Column(db.Integer, primary_key=True)
     name        = db.Column(db.String(), unique=True, nullable=False)
--- db_filldata.py
+++ db_filldata.py
@@ -7,13 +7,21 @@
 from app.config import SQLALCHEMY_DATABASE_URI
 from app import db
 import os.path
-from app.models import Faction, Star, Planet, Ship, Body
+import math.randint
+from app.models import Faction, Star, Planet, Ship, ShipClass, ShipPart, ShipPartClass, Body
+
+#def generateGalaxy():
+#    for i in range(1,100):
+#        db.session.add(Star('name '+i))
+
 
 def fillData():
     madulased=Faction(name="shshhh",race=u"madulased")
     inimesed=Faction(name="inimesed",race=u"humanoidid")
     db.session.add(madulased)
-    ship=Ship(name="Enterprise", faction=inimesed, body=Body(coordX=10, coordY=10))
+    ship=Ship(name="Enterprise", faction=inimesed, body=Body(coordX=10, coordY=10),
+                shipclass=ShipClass(name="Independence"),
+                shipparts=[ShipPart(shippartclass=ShipPartClass(name="engine"))])
     db.session.add(ship)
     db.session.commit()
 
