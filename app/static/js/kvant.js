27440c77aeca90c23823f4f0c4d92afa08a10e33
Madis Veskimeister <madis@percival.co.uk>
2014-10-11 01:10:17 +0300

    Removed spaced from JS, added loadMap.

--- app/static/js/kvant.js
+++ app/static/js/kvant.js
@@ -7,150 +7,189 @@ function initPaper() {
 }
 
 function loadStars() {
-    $.getJSON("/api/stars.json", function (json) {
-        console.log(json);
-        //var arr = $.parseJSON(json);
-        //$.each(json.allstars, function (star) {
-            //console.log(arr);
-            for (star in json.allstars) {
-                console.log(star);
-                console.log(star.name + " " + star.cx + " " + star.cy);
-            }
-        //});
-    });
+	$.getJSON("/api/stars.json", function (json) {
+		console.log(json);
+		//var arr = $.parseJSON(json);
+		//$.each(json.allstars, function (star) {
+			//console.log(arr);
+			for (star in json.allstars) {
+				console.log(star);
+				console.log(star.name + " " + star.cx + " " + star.cy);
+			}
+		//});
+	});
 }
 
 function loadStarXML(id) {
-    $.ajax({
-        type: "GET",
-    url: "/api/star"+id+".xml",
-    dataType: "xml",
-    success: function(xml) {
-        $(xml).find('star').each(function(){
-            var id = $(this).find('id').text();
-            var name = $(this).find('name').text();
-            var cx = $(this).find('cx').text();
-            var cy = $(this).find('cy').text();
-            var mass = $(this).find('mass').text();
-            $('#sidebar').html("Star: "+name+"<br>Coords: "+cx+":"+cy+"<br>Mass: "+mass);
-        });
-    }
-    });
+	$.ajax({
+		type: "GET",
+	url: "/api/star"+id+".xml",
+	dataType: "xml",
+	success: function(xml) {
+		$(xml).find('star').each(function(){
+			var id = $(this).find('id').text();
+			var name = $(this).find('name').text();
+			var cx = $(this).find('cx').text();
+			var cy = $(this).find('cy').text();
+			var mass = $(this).find('mass').text();
+			$('#sidebar').html("Star: "+name+"<br>Coords: "+cx+":"+cy+"<br>Mass: "+mass);
+		});
+	}
+	});
 }
 
 function loadShipXML(id) {
-    $.ajax({
-        type: "GET",
-    url: "/api/ship"+id+".xml",
-    dataType: "xml",
-    success: function(xml) {
-        $(xml).find('star').each(function(){
-            var id = $(this).find('id').text();
-            var name = $(this).find('name').text();
-            var cx = $(this).find('cx').text();
-            var cy = $(this).find('cy').text();
-            var mass = $(this).find('mass').text();
-            $('#sidebar').html("Star: "+name+"<br>Coords: "+cx+":"+cy+"<br>Mass: "+mass);
-        });
-    }
-    });
+	$.ajax({
+		type: "GET",
+	url: "/api/ship"+id+".xml",
+	dataType: "xml",
+	success: function(xml) {
+		$(xml).find('star').each(function(){
+			var id = $(this).find('id').text();
+			var name = $(this).find('name').text();
+			var cx = $(this).find('cx').text();
+			var cy = $(this).find('cy').text();
+			var mass = $(this).find('mass').text();
+			$('#sidebar').html("Star: "+name+"<br>Coords: "+cx+":"+cy+"<br>Mass: "+mass);
+		});
+	}
+	});
 }
 
 function loadStarsXML() {
-    $.ajax({
-        type: "GET",
-    url: "/api/stars.xml",
-    dataType: "xml",
-    success: function(xml) {
-        $(xml).find('star').each(function(){
-            var id = $(this).find('id').text();
-            var name = $(this).find('name').text();
-            var cx = $(this).find('cx').text();
-            var cy = $(this).find('cy').text();
-            var mass = $(this).find('mass').text();
-            //$('<div class="items" id="link_'+id+'"></div>').html('<a href="'+url+'">'+title+'</a>').appendTo('#page-wrap');
-            console.log(name + "-" + cx + "-" + cy);
-            var circle = paper.circle(cx*2.9, cy*1.9, 4);
-            var color = "#f77";
-            if (mass < 2000) { color = "#f88" } else
-            if (mass < 4000) { color = "#f99" } else
-            if (mass < 7000) { color = "#fbb" } else
-            if (mass < 9000) { color = "#fcc" } else
-            if (mass < 12000) { color = "#fdd" } else
-            if (mass < 18000) { color = "#adf" } else
-            if (mass < 22000) { color = "#aff" }
-            circle.attr("fill", color);
-            circle.attr("class","stars");
-            circle.click(function () {
+	$.ajax({
+		type: "GET",
+	url: "/api/stars.xml",
+	dataType: "xml",
+	success: function(xml) {
+		$(xml).find('star').each(function(){
+			var id = $(this).find('id').text();
+			var name = $(this).find('name').text();
+			var cx = $(this).find('cx').text();
+			var cy = $(this).find('cy').text();
+			var mass = $(this).find('mass').text();
+			//$('<div class="items" id="link_'+id+'"></div>').html('<a href="'+url+'">'+title+'</a>').appendTo('#page-wrap');
+			console.log(name + "-" + cx + "-" + cy);
+			var circle = paper.circle(cx*2.9, cy*1.9, 4);
+			var color = "#f77";
+			if (mass < 2000) { color = "#f88" } else
+			if (mass < 4000) { color = "#f99" } else
+			if (mass < 7000) { color = "#fbb" } else
+			if (mass < 9000) { color = "#fcc" } else
+			if (mass < 12000) { color = "#fdd" } else
+			if (mass < 18000) { color = "#adf" } else
+			if (mass < 22000) { color = "#aff" }
+			circle.attr("fill", color);
+			circle.attr("class","stars");
+			circle.click(function () {
 				if (aStar!= null) {
 					aStar.remove();
 				}
-                aStar = paper.text(cx*2.9-5, cy*1.9+7, name).attr({fill: "#f00"}).node.setAttribute("class","track"); //ellipse(cx*2.9, cy*1.9, 13, 8); //popup(cx*2.9, cy*1.9, "Laev", "left", 1);
-                loadStarXML(id);
-                console.log("klikk");
-            });
-        });
-    }
-    });
+				aStar = paper.text(cx*2.9-5, cy*1.9+7, name).attr({fill: "#f00"}).node.setAttribute("class","track"); //ellipse(cx*2.9, cy*1.9, 13, 8); //popup(cx*2.9, cy*1.9, "Laev", "left", 1);
+				loadStarXML(id);
+				console.log("klikk");
+			});
+		});
+	}
+	});
 }
 
 
+function loadMapXML() {
+	$.ajax({
+		type: "GET",
+	url: "/api/systems.xml",
+	dataType: "xml",
+	success: function(xml) {
+		$(xml).find('system').each(function(){
+			var star = $(this).find('star');
+			var id = $(star).find('id').text();
+			var name = $(star).find('name').text();
+			var cx = $(star).find('cx').text();
+			var cy = $(star).find('cy').text();
+			var mass = $(star).find('mass').text();
+			//$('<div class="items" id="link_'+id+'"></div>').html('<a href="'+url+'">'+title+'</a>').appendTo('#page-wrap');
+			console.log(name + "-" + cx + "-" + cy);
+			var circle = paper.circle(cx*2.9, cy*1.9, 4);
+			var color = "#f77";
+			if (mass < 2000) { color = "#f88" } else
+			if (mass < 4000) { color = "#f99" } else
+			if (mass < 7000) { color = "#fbb" } else
+			if (mass < 9000) { color = "#fcc" } else
+			if (mass < 12000) { color = "#fdd" } else
+			if (mass < 18000) { color = "#adf" } else
+			if (mass < 22000) { color = "#aff" }
+			circle.attr("fill", color);
+			circle.attr("class","stars");
+			circle.click(function () {
+				if (aStar!= null) {
+					aStar.remove();
+				}
+				aStar = paper.text(cx*2.9-5, cy*1.9+7, name).attr({fill: "#f00"}).node.setAttribute("class","track"); //ellipse(cx*2.9, cy*1.9, 13, 8); //popup(cx*2.9, cy*1.9, "Laev", "left", 1);
+				loadStarXML(id);
+				console.log("klikk");
+			});
+		});
+	}
+	});
+}
+
 function loadShipsXML() {
-    $.ajax({
-        type: "GET",
-    url: "/api/ships.xml",
-    dataType: "xml",
-    success: function(xml) {
+	$.ajax({
+		type: "GET",
+	url: "/api/ships.xml",
+	dataType: "xml",
+	success: function(xml) {
 		var i = 1;
-        $(xml).find('ship').each(function(){
-            var id = $(this).find('id').text();
-            var name = $(this).find('name').text();
-            var cx = $(this).find('cx').text();
-            var cy = $(this).find('cy').text();
-            var mass = $(this).find('mass').text();
-            //$('<div class="items" id="link_'+id+'"></div>').html('<a href="'+url+'">'+title+'</a>').appendTo('#page-wrap');
-            console.log(name + "-" + cx + "-" + cy);
-            var circle = paper.circle(30, i*30, 20, 4);
-            var color = "#f77";
-            circle.attr("fill", color);
-            var silt1 = paper.text(80, i*30, name).attr({fill: "#f00"});
-            var silt2 = paper.text(180, i*30, mass).attr({fill: "#f00"});
-            var silt3 = paper.text(240, i*30, mass).attr({fill: "#f00"});
-            var ellip = paper.ellipse(37, i*30, 13, 8);
-            //var popeye = paper.popup(i*230, 20, "Laev", "left", 1);
-            i++;
-        });
-    }
-    });
+		$(xml).find('ship').each(function(){
+			var id = $(this).find('id').text();
+			var name = $(this).find('name').text();
+			var cx = $(this).find('cx').text();
+			var cy = $(this).find('cy').text();
+			var mass = $(this).find('mass').text();
+			//$('<div class="items" id="link_'+id+'"></div>').html('<a href="'+url+'">'+title+'</a>').appendTo('#page-wrap');
+			console.log(name + "-" + cx + "-" + cy);
+			var circle = paper.circle(30, i*30, 20, 4);
+			var color = "#f77";
+			circle.attr("fill", color);
+			var silt1 = paper.text(80, i*30, name).attr({fill: "#f00"});
+			var silt2 = paper.text(180, i*30, mass).attr({fill: "#f00"});
+			var silt3 = paper.text(240, i*30, mass).attr({fill: "#f00"});
+			var ellip = paper.ellipse(37, i*30, 13, 8);
+			//var popeye = paper.popup(i*230, 20, "Laev", "left", 1);
+			i++;
+		});
+	}
+	});
 }
 
 function loadImperiumXML() {
-    $.ajax({
-        type: "GET",
-    url: "/api/imperium.xml",
-    dataType: "xml",
-    success: function(xml) {
-        $(xml).find('ship').each(function(){
-            var id = $(this).find('id').text();
-            var name = $(this).find('name').text();
-            var cx = $(this).find('cx').text();
-            var cy = $(this).find('cy').text();
-            var mass = $(this).find('mass').text();
-            //$('<div class="items" id="link_'+id+'"></div>').html('<a href="'+url+'">'+title+'</a>').appendTo('#page-wrap');
-            console.log(name + "-" + cx + "-" + cy);
-            dashed = {fill: "none", stroke: "#666", "stroke-dasharray": "- "};
-            var circle1 = paper.ellipse(cx*2.9, cy*1.9, 5, 3);
-            var circle2 = paper.path("M10"+cx*2.9 + " " + cy*1.9 +" "+ 4).attr(dashed); //circle(cx*2.9, cy*1.9, 4);
-            var color = "#779";
-            circle1.attr("fill", color);
-            circle2.attr("fill", color);
-            circle1.click(function () {
-                console.log("klikk");
-                aStar = paper.text(cx*2.9-5, cy*1.9+7, name).attr({fill: "#f00"}); //ellipse(cx*2.9, cy*1.9, 13, 8); //popup(cx*2.9, cy*1.9, "Laev", "left", 1);
-            });
-        });
-    }
-    });
+	$.ajax({
+		type: "GET",
+	url: "/api/imperium.xml",
+	dataType: "xml",
+	success: function(xml) {
+		$(xml).find('ship').each(function(){
+			var id = $(this).find('id').text();
+			var name = $(this).find('name').text();
+			var cx = $(this).find('cx').text();
+			var cy = $(this).find('cy').text();
+			var mass = $(this).find('mass').text();
+			//$('<div class="items" id="link_'+id+'"></div>').html('<a href="'+url+'">'+title+'</a>').appendTo('#page-wrap');
+			console.log(name + "-" + cx + "-" + cy);
+			dashed = {fill: "none", stroke: "#666", "stroke-dasharray": "- "};
+			var circle1 = paper.ellipse(cx*2.9, cy*1.9, 5, 3);
+			var circle2 = paper.path("M10"+cx*2.9 + " " + cy*1.9 +" "+ 4).attr(dashed); //circle(cx*2.9, cy*1.9, 4);
+			var color = "#779";
+			circle1.attr("fill", color);
+			circle2.attr("fill", color);
+			circle1.click(function () {
+				console.log("klikk");
+				aStar = paper.text(cx*2.9-5, cy*1.9+7, name).attr({fill: "#f00"}); //ellipse(cx*2.9, cy*1.9, 13, 8); //popup(cx*2.9, cy*1.9, "Laev", "left", 1);
+			});
+		});
+	}
+	});
 }
 
 function displayShips() {
@@ -168,7 +207,7 @@ function displayShips() {
 
 function displayStars() {
 	initPaper();
-	loadStarsXML();
+	loadMapXML();
 	loadImperiumXML();
 	var bot = paper.bottom, res = [];
 	while (bot) {
@@ -195,6 +234,6 @@ function displayPlanets() {
 }
 
 $(document).ready(function()  {
-    displayStars();
-    //loadImperiumXML();
+	displayStars();
+	//loadImperiumXML();
 });
