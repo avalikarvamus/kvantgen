e45c8e7d94354acd0f325fec979a0c155dbf3e37
Madis Veskimeister <madis@pingviinitiivul.ee>
2015-11-22 14:15:41 +0200

    Removed XML things and unused old things.

--- app/static/js/kvant2.js
+++ app/static/js/kvant2.js
@@ -156,13 +156,13 @@ function loadMap() {
 }
 
 
-function loadMapXML() {
+function loadMap() {
 	$.ajax({
 		type: "GET",
-	url: "/api/systems.xml",
-	dataType: "xml",
-	success: function(xml) {
-		$(xml).find('system').each(function(){
+	url: "/api/systems/",
+	dataType: "json",
+	success: function(json) {
+		$(json).find('system').each(function(){
 			var star = $(this).find('star');
 			var id = $(star).find('id').text();
 			var name = $(star).find('name').text();
@@ -219,64 +219,6 @@ function loadMapXML() {
 	});
 }
 
-function loadShipsXML() {
-	$.ajax({
-		type: "GET",
-	url: "/api/ships.xml",
-	dataType: "xml",
-	success: function(xml) {
-		var i = 1;
-		$(xml).find('ship').each(function(){
-			var id = $(this).find('id').text();
-			var name = $(this).find('name').text();
-			var cx = $(this).find('cx').text();
-			var cy = $(this).find('cy').text();
-			var mass = $(this).find('mass').text();
-			//$('<div class="items" id="link_'+id+'"></div>').html('<a href="'+url+'">'+title+'</a>').appendTo('#page-wrap');
-			console.log(name + "-" + cx + "-" + cy);
-			var circle = paper.circle(30, i*30, 20, 4);
-			var color = "#f77";
-			circle.attr("fill", color);
-			var silt1 = paper.text(80, i*30, name).attr({fill: "#f00"});
-			var silt2 = paper.text(180, i*30, mass).attr({fill: "#f00"});
-			var silt3 = paper.text(240, i*30, mass).attr({fill: "#f00"});
-			var ellip = paper.ellipse(37, i*30, 13, 8);
-			//var popeye = paper.popup(i*230, 20, "Laev", "left", 1);
-			i++;
-		});
-	}
-	});
-}
-
-function loadImperiumXML() {
-	$.ajax({
-		type: "GET",
-	url: "/api/imperium.xml",
-	dataType: "xml",
-	success: function(xml) {
-		$(xml).find('ship').each(function(){
-			var id = $(this).find('id').text();
-			var name = $(this).find('name').text();
-			var cx = $(this).find('cx').text();
-			var cy = $(this).find('cy').text();
-			var mass = $(this).find('mass').text();
-			//$('<div class="items" id="link_'+id+'"></div>').html('<a href="'+url+'">'+title+'</a>').appendTo('#page-wrap');
-			console.log(name + "-" + cx + "-" + cy);
-			dashed = {fill: "none", stroke: "#666", "stroke-dasharray": "- "};
-			var circle1 = paper.ellipse(10+cx*8.4, 10+cy*5.8, 5, 3);
-			var circle2 = paper.path("M10"+10+cx*8.4 + " " + 10+cy*5.8 +" "+ 4).attr(dashed); //circle(cx*2.9, cy*1.9, 4);
-			var color = "#779";
-			circle1.attr("fill", color);
-			circle2.attr("fill", color);
-			circle1.click(function () {
-				console.log("klikk");
-				aStar = paper.text(10+cx*8.4-5, 10+cy*5.8+7, name).attr({fill: "#f00"}); //ellipse(cx*2.9, cy*1.9, 13, 8); //popup(cx*2.9, cy*1.9, "Laev", "left", 1);
-			});
-		});
-	}
-	});
-}
-
 function loadLeadersJSON() {
 	$.ajax({
 		type: "GET",
@@ -318,8 +260,8 @@ function displayShips() {
 
 function displayStars() {
 	initPaper();
-	loadMapXML();
-	loadImperiumXML();
+	loadMap();
+	//loadImperium();
 	var bot = paper.bottom, res = [];
 	while (bot) {
 		//res.push(bot);
