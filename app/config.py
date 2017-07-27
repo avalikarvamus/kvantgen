a7641608455467a25b1dbd4a08202cbd92618db1
Madis Veskimeister <madis@pingviinitiivul.ee>
2017-07-28 00:36:56 +0300

    Updated some dependencies. Switched Track Modifications On.

--- app/config.py
+++ app/config.py
@@ -7,3 +7,4 @@ TESTING = True
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = True
--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#    
+#
 #    Copyright 2013 Madis Veskimeister <madis@pingviinitiivul.ee>
 #
 
-from flask.ext.login import LoginManager
+from flask_login import LoginManager
 from app import app
 
 login_manager = LoginManager()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug='True')
-    
